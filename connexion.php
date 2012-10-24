b4a0fb6e9b0278771f04c24b33d73b9e9d8935fc
Justn Lavoie <jlavoie1602@gmail.com>
2012-10-24 14:26:30 -0400

    Ajout de la création de cookie

--- connexion.php
+++ connexion.php
@@ -3,7 +3,7 @@ session_start();
 
 require('param_bd.inc');
 
-if(isset($_POST['login']) AND isset($_POST['passwd']) AND $_POST['login'] != null AND $_POST['passwd'] != null)
+if(isset($_POST['username']) AND isset($_POST['password']) AND $_POST['username'] != null AND $_POST['password'] != null)
 {
 	$bd = new pdo('mysql:host='.$dbHote.';dbname='.$dbName, $dbUser, $dbPass);
 
@@ -11,24 +11,30 @@ if(isset($_POST['login']) AND isset($_POST['passwd']) AND $_POST['login'] != nul
 
 	$req->execute(
 		array(
-			'login' => $_POST['login'],
-			'passwd' => sha1($_POST['passwd'])
+			'login' => $_POST['username'],
+			'passwd' => sha1($_POST['password'])
 		));
 
 	$data = $req->fetchAll();
 
 	if(isset($data) AND $data != null)
 	{
+		// Garder les variables de session
 		$_SESSION['login'] = $data['login'];
 		$_SESSION['id'] = $data['id'];
 		$_SESSION['acces'] = $data['acces'];
+		// Si cocher, créer un cookie pour
+		// se souvenir de l'utilisateur
+			
+		{
+			setcookie('cmsrl_remember', $data['id'], (time() + 3600), '/');
+		}
 		echo true;
 	}
 	else
 	{
 		echo "Nom d'utilisateur ou mot de passe invalide.";
 	}
-
 }
 
 
