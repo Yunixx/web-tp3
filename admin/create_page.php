ddf33baaf5f2463fd8576c22889525dc2d672b55
Justn Lavoie <jlavoie1602@gmail.com>
2012-10-24 15:53:32 -0400

    Create page begin implementation

--- admin/create_page.php
+++ admin/create_page.php
@@ -1,5 +1,9 @@
 <?php
+session_start();
+require('../param_bd.inc');
 
+$db = new pdo('mysql:host='.$dbHote.'dbname='.$dbName, $dbUser, $dbPass);
 
+$req = $db->prepare('INSERT INTO articles(titre, content, user_id, sujet_id) VALUE()')
 
 ?>
