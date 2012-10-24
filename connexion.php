e0e5bd52fbe96346cd3e1bb4fb6f59b6fe39d6bb
Justn Lavoie <jlavoie1602@gmail.com>
2012-10-24 14:01:22 -0400

    Implémentation du script de connexion

--- connexion.php
+++ connexion.php
@@ -1,9 +1,34 @@
 <?php
 session_start();
 
-if(isset($_POST['username']) AND isset($_POST['passwd']) AND $_POST['username'] != null AND $_POST['passwd'] != null)
+require('param_bd.inc');
+
+if(isset($_POST['login']) AND isset($_POST['passwd']) AND $_POST['login'] != null AND $_POST['passwd'] != null)
 {
-	$bd = new pdo()
+	$bd = new pdo('mysql:host='.$dbHote.';dbname='.$dbName, $dbUser, $dbPass);
+
+	$req = $bd->prepare("SELECT * FROM usagers WHERE login = :login AND enc_password = :passdw");
+
+	$req->execute(
+		array(
+			'login' => $_POST['login'],
+			'passwd' => sha1($_POST['passwd'])
+		));
+
+	$data = $req->fetchAll();
+
+	if(isset($data) AND $data != null)
+	{
+		$_SESSION['login'] = $data['login'];
+		$_SESSION['id'] = $data['id'];
+		$_SESSION['acces'] = $data['acces'];
+		echo true;
+	}
+	else
+	{
+		echo "Nom d'utilisateur ou mot de passe invalide.";
+	}
+
 }
 
 
