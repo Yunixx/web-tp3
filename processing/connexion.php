68638eb9e05771c92f1153474e76b9a8717c5001
Justn Lavoie <jlavoie1602@gmail.com>
2012-10-25 08:55:31 -0400

    Use garneau user

--- processing/connexion.php
+++ processing/connexion.php
@@ -5,8 +5,7 @@ require('param_bd.inc');
 
 if(isset($_POST['username']) AND isset($_POST['password']) AND $_POST['username'] != NULL AND $_POST['password'] != NULL)
 {
-	//$bd = new pdo('mysql:host='.$dbHote.';dbname='.$dbName, $dbUser, $dbPass);
-	$bd = new pdo('mysql:host=localhost;dbname=cmsrl' ,'root', 'root');
+	$bd = new pdo('mysql:host='.$dbHote.';dbname='.$dbName, $dbUser, $dbPass);
 
 	$req = $bd->prepare("SELECT * FROM usagers WHERE login = :username AND enc_password = :password");
 
