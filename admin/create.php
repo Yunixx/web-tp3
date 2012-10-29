d4a91fc55cab306396ce96feac662606d1d9ef2f
Justn Lavoie <jlavoie1602@gmail.com>
2012-10-29 18:10:47 -0400

    Begin form implementation

--- admin/create.php
+++ admin/create.php
@@ -5,8 +5,11 @@ require('../lib/connect_db.inc');
 
 <div id="create_form" title="Create a new page">
 	<form method="post" action="processing/create_page.php" />
-		<input type="text" id="titre" name="titre" placeholder="Titre de l'article" /><br/>
+		<label for="titre">Titre<label><b4/>
+		<input type="text" id="titre" name="titre" placeholder="Titre" /><br/>
+		<label for="content">Contenu</label>
 		<textarea id="content" name="content" placeholder="Contenu de votre article" cols="43" rows="10"></textarea><br/>
+		<label for="username">Nom d'utilisteur</label>
 		<?php if($_SESSION['admin'] == "admin")
 		{
 			// mettre le checkbox pour réduire l'acces et la drop down list avec seulement les admins
---
+++ admin/delete.php
@@ -0,0 +1,7 @@
+<form method="post" action"processing/delete_page.php">
+	<?php
+		/**
+		 * Mettre la dropdown liste de tous les articles que L,utilisateur connecté à créés.
+		 **/
+		?>
+</form>
