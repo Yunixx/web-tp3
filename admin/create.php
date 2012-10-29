2b3e55d5006c85f706123fb38ab352e1f36b9143
Justn Lavoie <jlavoie1602@gmail.com>
2012-10-29 17:54:05 -0400

    Implement create form

--- admin/create.php
+++ admin/create.php
@@ -1,8 +1,20 @@
-<div id="create_page" title="Create a new page">
-	<form method="post" action="admin/processing/create_page.php" />
+<?php 
+session_start();
+require('../lib/connect_db.inc');
+?>
+
+<div id="create_form" title="Create a new page">
+	<form method="post" action="processing/create_page.php" />
 		<input type="text" id="titre" name="titre" placeholder="Titre de l'article" /><br/>
 		<textarea id="content" name="content" placeholder="Contenu de votre article" cols="43" rows="10"></textarea><br/>
-		<input type="checkbox" id="acces" name="acces" /><label for="acces">Restreindre</label><br/>
+		<?php if($_SESSION['admin'] == "admin")
+		{
+			// mettre le checkbox pour réduire l'acces et la drop down list avec seulement les admins
+			<input type="checkbox" id="acces" name="acces" /><label for="acces">Restreindre</label><br/>
+		}else
+		{
+			// mettre la drop down list avec les membres et les admins
+		}
 		<input type="submit" value="Créer la page" />
 	</form>
 
