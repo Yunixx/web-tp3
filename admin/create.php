eaf9163c51a9e0720101b8a6227d7b875b1d1a4f
Justn Lavoie <jlavoie1602@gmail.com>
2012-10-31 19:22:41 -0400

    Add session_start()

--- admin/create.php
+++ admin/create.php
@@ -1,10 +1,15 @@
-<?php require('../lib/header.inc'); ?>
+<?php 
+session_start();
+require('../lib/header.inc'); 
+?>
 <section id="main"><!-- #main content area -->
 <p class="form_title">Créer une nouvelle page</p>
 	<div id="create_form" title="Create a new page">
 		<form method="post" action="processing/create_page.php" />
 			<label for="titre">Titre: <label><br/>
 			<input type="text" id="titre" name="titre" placeholder="Titre" /><br/>
+			<label for="menu_titre">Menu Titre:</label><br/>
+			<input type="text" id="menu_titre" name="menu_titre" placeholder="Menu titre" /><br/>
 			<label for="content">Contenu: </label><br/>
 			<textarea id="content" name="content" placeholder="Contenu de votre article"></textarea><br/>
 			<label for="username">Nom d'utilisteur: </label><br/>
@@ -12,7 +17,7 @@
 				/*if($_SESSION['admin'] == "admin")
 				{
 					// mettre le checkbox pour réduire l'acces et la drop down list avec seulement les admins
-					echo '<input type="checkbox" id="acces" name="acces" /><label for="acces">Restreindre</label><br/>';'
+					echo '<input type="checkbox" id="acces" name="acces" /><label for="acces">Restreindre</label><br/>';
 				}else
 				{
 					// mettre la drop down list avec les membres et les admins
