1230e346699e87e2573d0b38f45e384b3db9ca1d
Justn Lavoie <jlavoie1602@gmail.com>
2012-11-01 10:30:29 -0400

    Add tiny mce

--- admin/create.php
+++ admin/create.php
@@ -1,5 +1,12 @@
 <p class="form_title">Créer une nouvelle page</p>
 	<div id="create_form" title="Create a new page">
+<script type="text/javascript" src="js/tiny_mce/tiny_mce.js"></script>
+<script type="text/javascript">
+tinyMCE.init({
+	mode : "textareas",
+	theme : "advanced"
+});
+</script>
 		<form method="post" action="admin/processing/create_page.php" />
 			<label for="titre">Titre: <label><br/>
 			<input type="text" id="titre" name="titre" placeholder="Titre" /><br/>
