a455dd7cd3961164f4990da95e8999191b4eeb70
Justn Lavoie <jlavoie1602@gmail.com>
2012-11-01 08:43:58 -0400

    Change admin menu content

--- admin/menu_admin.php
+++ admin/menu_admin.php
@@ -12,10 +12,9 @@ else
  ?>!</h4>
     		
     		<ul>
-    		<a href="#"><li>- <span id="addArticle">Ajouter un article</span></li></a>
-    			<a href="#"><li>- <span id="editArticle">Gestions des articles</span></li></a>
-    			<a href="#"><li>- <span id="addPage">Ajouter une page</span></li></a>
-    			<a href="#"><li>- <span id="editPage">Éditer une page</span></li></a>
-    			<a href="#"><li>- <span id="editUser">Gestion des utilisateurs</span></li></a>
+    			<a href="admin/create.php"><li>- <span id="addPage">Ajouter une page</span></li></a>
+    			<a href="admin/manage.php"><li>- <span id="editPage">Éditer une page</span></li></a>
+    			<a href="admin/delete.php"><li>- <span id="delPage">Supprimer des articles</span></li></a>
+    			<a href=""><li>- <span id="editUser">Gestion des utilisateurs</span></li></a>
     		</ul>
     		<a href="#"><span id="lienDeco">Déconnexion</span></a>
