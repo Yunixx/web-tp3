0aa1c3fb34eb6b3f402ca929b14012fa25e44d31
Jonathan Roy <jonathanroy20@msn.com>
2012-11-01 08:29:55 -0400

    Revert "Revert "Merge pull request #8 from just1602/dev""
    This reverts commit f44cc9ba974a99dc91f3f2b3ab19449e65422860.

---
+++ admin/manage.php
@@ -0,0 +1,24 @@
+<form method="pôst" action="processing/manage_page.php">
+	<?php
+		/**
+		 * Mettre une dropdown liste avec les articles 
+		 * et changer le contenu du form à l'aide de ajax
+		 * dépendant la page choisie
+		 **/
+	?>
+
+	<label for="titre">Titre</label><br/>
+	<input type="text" id="titre" name="titre" placeholder="titre" /><br/>
+	<label for="content">Contenu</label><br/>
+	<textarea id="content" name="content" placeholder="Contenu de votre page"></textarea><br/>
+	<!-- datepicker jquery ui -->
+	<label for="acces">Accès</label>
+	<?php 
+		if($_SESSION['acces'] == "admin")
+		{
+			<input type="checkbox" id="acces" name="acces" />
+		}
+	?>
+	<input type="submit" value="Éditer" />
+
+</form>
